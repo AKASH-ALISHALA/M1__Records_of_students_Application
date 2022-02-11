5a878d45bae2cbb76579f6ec9d1c0407b08d0438
AKASH-ALISHALA <akashalishala@gmail.com>
2022-02-11 15:22:09 +0530

    Commit

---
+++ 3_Implementation/display.h
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+
+void display_all_records()
+{
+    FILE *fp;
+    struct Stu s;
+    fp=fopen("MINI_PROJECT_IN_C.DAT","rb");
+    printf("\n--------------------------------------------");
+    printf("\t\t Records of all students\n\n");
+    printf("\n--------------------------------------------");
+    while(1)
+    {
+        fread(&s,sizeof(s),1,fp);
+        if(feof(fp))
+        {
+            break;
+        }
+        printf("\nrollno:%d\t",s.rollno);
+        printf("nm:%s\t",s.nm);
+        printf("age:%d\t",s.age);
+        printf("sum:%d\n",s.sum);
+    }
+    printf("\n--------------------------------------------");
+    fclose(fp);
+}
